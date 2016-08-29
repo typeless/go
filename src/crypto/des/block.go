dcee002c5389dea089a6fd78ae03521f57c22fbd
Mura Li <mura_li@castech.com.tw>
2016-08-29 10:22:20 +0800

    crypto/des: improve the throughput of DES
    DO NOT REVIEW
    
    The basic idea is inspired by common C implementations (e.g. https://tls.mbed.org/ and CyaSSL)
    with certain deviations for minimizing the changes to the existing Go implementation.
    
    There are remaining works which can probably improve the performance further.
    
    For example, the Feistiel function in C does not need to perform the 1 bit
    left-rotations and the 29 bit right-rotations to the input blocks before
    XORed with the subkeys.
    
    Benchmark results:
    
    Intel Core i5 4460 3.2GHz:
    benchmark               old ns/op     new ns/op     delta
    BenchmarkEncrypt-16     504           159           -68.45%
    BenchmarkDecrypt-16     504           158           -68.65%
    
    benchmark               old MB/s     new MB/s     speedup
    BenchmarkEncrypt-16     15.87        50.13        3.16x
    BenchmarkDecrypt-16     15.85        50.47        3.18x
    
    ARM1136 133MHz:
    benchmark            old ns/op     new ns/op     delta
    BenchmarkEncrypt     60054         15844         -73.62%
    BenchmarkDecrypt     59669         15797         -73.53%
    
    benchmark            old MB/s     new MB/s     speedup
    BenchmarkEncrypt     0.13         0.50         3.85x
    BenchmarkDecrypt     0.13         0.51         3.92x
    
    Change-Id: I408eb0f23ca4abc3c5b9d694477c83b79bf255af

--- src/crypto/des/block.go
+++ src/crypto/des/block.go
@@ -4,25 +4,23 @@
 
 package des
 
-import (
-	"encoding/binary"
-)
+import "encoding/binary"
 
 func cryptBlock(subkeys []uint64, dst, src []byte, decrypt bool) {
 	b := binary.BigEndian.Uint64(src)
 	b = permuteInitialBlock(b)
 	left, right := uint32(b>>32), uint32(b)
 
-	var subkey uint64
-	for i := 0; i < 16; i++ {
-		if decrypt {
-			subkey = subkeys[15-i]
-		} else {
-			subkey = subkeys[i]
+	if decrypt {
+		for i := 0; i < 8; i++ {
+			left, right = feistel(left, right, subkeys[15-2*i], subkeys[15-(2*i+1)])
 		}
-
-		left, right = right, left^feistel(right, subkey)
+	} else {
+		for i := 0; i < 8; i++ {
+			left, right = feistel(left, right, subkeys[2*i], subkeys[2*i+1])
+		}
 	}
+
 	// switch left & right and perform final permutation
 	preOutput := (uint64(right) << 32) | uint64(left)
 	binary.BigEndian.PutUint64(dst, permuteFinalBlock(preOutput))
@@ -39,19 +37,38 @@ func decryptBlock(subkeys []uint64, dst, src []byte) {
 }
 
 // DES Feistel function
-func feistel(right uint32, key uint64) (result uint32) {
-	sBoxLocations := key ^ expandBlock(right)
-	var sBoxResult uint32
-	for i := uint8(0); i < 8; i++ {
-		sBoxLocation := uint8(sBoxLocations>>42) & 0x3f
-		sBoxLocations <<= 6
-		// row determined by 1st and 6th bit
-		// column is middle four bits
-		row := (sBoxLocation & 0x1) | ((sBoxLocation & 0x20) >> 4)
-		column := (sBoxLocation >> 1) & 0xf
-		sBoxResult ^= feistelBox[i][16*row+column]
-	}
-	return sBoxResult
+func feistel(l, r uint32, k0, k1 uint64) (lout, rout uint32) {
+	var t uint32
+
+	t = ((r << 29) | (r >> 3)) ^ uint32(k0)
+
+	l ^= feistelBox[6][(t)&0x3f] ^
+		feistelBox[4][(t>>8)&0x3f] ^
+		feistelBox[2][(t>>16)&0x3f] ^
+		feistelBox[0][(t>>24)&0x3f]
+
+	t = ((r << 1) | (r >> 31)) ^ uint32(k0>>32)
+
+	l ^= feistelBox[7][t&0x3f] ^
+		feistelBox[5][(t>>8)&0x3f] ^
+		feistelBox[3][(t>>16)&0x3f] ^
+		feistelBox[1][(t>>24)&0x3f]
+
+	t = ((l << 29) | (l >> 3)) ^ uint32(k1)
+
+	r ^= feistelBox[6][(t)&0x3f] ^
+		feistelBox[4][(t>>8)&0x3f] ^
+		feistelBox[2][(t>>16)&0x3f] ^
+		feistelBox[0][(t>>24)&0x3f]
+
+	t = ((l << 1) | (l >> 31)) ^ uint32(k1>>32)
+
+	r ^= feistelBox[7][t&0x3f] ^
+		feistelBox[5][(t>>8)&0x3f] ^
+		feistelBox[3][(t>>16)&0x3f] ^
+		feistelBox[1][(t>>24)&0x3f]
+
+	return l, r
 }
 
 // feistelBox[s][16*i+j] contains the output of permutationFunction
@@ -73,25 +90,16 @@ func init() {
 			for j := 0; j < 16; j++ {
 				f := uint64(sBoxes[s][i][j]) << (4 * (7 - uint(s)))
 				f = permuteBlock(f, permutationFunction[:])
-				feistelBox[s][16*i+j] = uint32(f)
+
+				// row determined by 1st and 6th bit
+				// column is middle four bits
+				row := uint8(((i & 2) << 4) | i&1)
+				col := uint8(j << 1)
+				t := row | col
+				feistelBox[s][t] = uint32(f)
 			}
 		}
 	}
-}
-
-// expandBlock expands an input block of 32 bits,
-// producing an output block of 48 bits.
-func expandBlock(src uint32) (block uint64) {
-	// rotate the 5 highest bits to the right.
-	src = (src << 5) | (src >> 27)
-	for i := 0; i < 8; i++ {
-		block <<= 6
-		// take the 6 bits on the right
-		block |= uint64(src) & (1<<6 - 1)
-		// advance by 4 bits.
-		src = (src << 4) | (src >> 28)
-	}
-	return
 }
 
 // permuteInitialBlock is equivalent to the permutation defined
@@ -218,6 +226,27 @@ func (c *desCipher) generateSubkeys(keyBytes []byte) {
 		// combine halves to form 56-bit input to PC2
 		pc2Input := uint64(leftRotations[i])<<28 | uint64(rightRotations[i])
 		// apply PC2 permutation to 7 byte input
-		c.subkeys[i] = permuteBlock(pc2Input, permutedChoice2[:])
+		c.subkeys[i] = unpack48bits(permuteBlock(pc2Input, permutedChoice2[:]))
 	}
+}
+
+// Expand the 48 bits key to 64 bits, with echo 6 bits block padded by extra two bits at left.
+// By doing so, we can have the input blocks (4 bits each), and the key blocks (6 bits each) well-aligned without
+// doing extra shifts/rotations for aligning bit positions. Also by having the key blocks (6 bits each) grouped with
+// odd and even ones, all 8 input and key blocks can be XORed in one 'pack' without overlapping.
+// In summary, this is kinda (bitwise) data layout rearrangement for enabling SIMD (operating multiple sub-word blocks
+// in one instruction).
+func unpack48bits(x uint64) uint64 {
+	var result uint64
+
+	result = ((x>>(6*1))&0xff)<<(8*0) |
+		((x>>(6*3))&0xff)<<(8*1) |
+		((x>>(6*5))&0xff)<<(8*2) |
+		((x>>(6*7))&0xff)<<(8*3) |
+		((x>>(6*0))&0xff)<<(8*4) |
+		((x>>(6*2))&0xff)<<(8*5) |
+		((x>>(6*4))&0xff)<<(8*6) |
+		((x>>(6*6))&0xff)<<(8*7)
+
+	return result
 }
